90b3eee214e9a3f5f124efa88852e16aebc4f7c8
Chetan Singh <boyzingsing@gmail.com>
2023-01-15 22:48:37 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+       // obj.name = "chetan";
+        obj.setName("chetan");
+        obj.getName();
+    }
 }
